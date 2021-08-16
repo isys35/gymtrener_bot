e7038991ad691945d43730d6824301b8b1614407
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-08-16 22:49:36 +0300

    init

--- webhook/views.py
+++ webhook/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from rest_framework import status
+from rest_framework.response import Response
 
-# Create your views here.
+
+async def webhook(request):
+    if request.method == 'POST':
+        return Response("Hello, async Django!")
+    else:
+        return Response({'code': 405}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
