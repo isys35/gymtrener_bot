5a8759cd95490011ce25c2dee9b5b587b14968bd
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-08-19 20:14:26 +0300

    init telegram context

---
+++ telegram_bot/core/telegram_context.py
@@ -0,0 +1,34 @@
+import telebot
+
+
+class TelegramContext:
+    bot = None
+
+    def __init__(self, token: str):
+        """
+        Инициализирует telebot.TeleBot(token)
+        :param token: токен бота в Telegram
+        """
+        self.bot = telebot.TeleBot(token, threaded=False)
+
+    def send_message(self,
+                     receiver: int,
+                     text: str,
+                     markup: telebot.types.ReplyKeyboardMarkup = None) -> None:
+        """
+        Отправляет сообщение пользователю в Telegram.
+        :param receiver: id пользователя в Telegram
+        :param text: строка с сообщением
+        :param markup: клавиатура для показа пользователю
+        :return: None
+        """
+        kwargs = {
+            'chat_id': receiver,
+            'text': text,
+            'disable_web_page_preview': True,
+            'disable_notification': True,
+            'parse_mode': 'HTML',
+            'reply_markup': markup,
+            'timeout': 1
+        }
+        self.bot.send_message(**kwargs)
