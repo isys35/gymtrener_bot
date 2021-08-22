dbc11f756c4bd40776624528b9300e49e7d817af
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-08-22 21:11:25 +0300

    init keyboard

---
+++ telegram_bot/keyboard.py
@@ -0,0 +1,52 @@
+import math
+
+from telebot import types
+
+
+class State:
+    __state = None
+
+    @property
+    def state(self):
+        return self.__state
+
+    def __init__(self, state):
+        self.__state = state
+        self.__state.context = self
+
+    def transition_to(self, state):
+        self.__state = state
+        self.__state.context = self
+
+
+def keyboard(method):
+    def decorator(self, *args):
+        self.kb_data = []
+        method(self, *args)
+        return self.get_keyboard(self.kb_data)
+
+    return decorator
+
+
+class BotKeyboard(State):
+    kb_data = None
+
+    def row(self, *args):
+        button_row = [*args]
+        self.kb_data.append(button_row)
+
+    def get_keyboard(self, buttons):
+        """
+        Вызывает одноименную функцию из созданного state
+        :param buttons:
+        :return:
+        """
+        return self.state.get_keyboard(buttons)
+
+    @keyboard
+    def main(self) -> None:
+        """
+        Функция генерации клавиатуры главного меню.
+        :return: None
+        """
+        self.row('\U0001F4B1 Кнопка', '\U0001F4B3 Кнопка')
