ccfbd2207c31970e6f35692cf44689bf07dfdae5
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-08-29 21:24:15 +0300

    add test welcome view

--- webhook/tests.py
+++ webhook/tests.py
@@ -1,5 +1,27 @@
 from django.test import TestCase
 
+from telegram_bot.views import welcome
+
+
+class KeyboardMock:
+    def main(self):
+        return
+
+
 class BotMock:
-    def send_message(**kwargs):
-        pass
+    keyboard = KeyboardMock()
+    text_message = None
+
+    def send_message(self, text, keyboard):
+        self.text_message = text
+
+
+class WelcomeViewTest(TestCase):
+
+    def setUp(self) -> None:
+        self.bot = BotMock()
+
+    def test_welcome(self):
+        welcome(self.bot)
+        text_message = self.bot.text_message
+        self.assertEqual("Привет мир", text_message)
