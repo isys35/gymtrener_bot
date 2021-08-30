01d7fcde2aa9eef7cad7d5c3fa190a82ccce8164
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-08-30 20:48:42 +0300

    add test User model

--- webhook/tests.py
+++ webhook/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 
+from telegram_bot.user import User
 from telegram_bot.views import welcome
 
 
@@ -16,6 +17,10 @@ class BotMock:
         self.text_message = text
 
 
+class UpdateMock:
+    data = {'message': {'user': {'id': 0}, 'text': 'TEST'}}
+
+
 class WelcomeViewTest(TestCase):
 
     def setUp(self) -> None:
@@ -25,3 +30,16 @@ class WelcomeViewTest(TestCase):
         welcome(self.bot)
         text_message = self.bot.text_message
         self.assertEqual("Привет мир", text_message)
+
+
+class UserModelTest(TestCase):
+    def setUp(self) -> None:
+        self.update = UpdateMock()
+
+    def test_init_from_update(self):
+        user = User(self.update)
+        user.init_from_update()
+        self.assertEqual(0, user.id)
+        self.assertEqual('TEST', user.full_request)
+        self.assertEqual(self.update, user.update)
+        self.assertEqual('/', user.state)
