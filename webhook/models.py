7aed50fd6edd6183d1bed2b5441c59bc5b392fbe
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-08-31 21:05:42 +0300

    add first models and init migrations

--- webhook/models.py
+++ webhook/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class TelegramUser(models.Model):
+    first_name = models.CharField(max_length=100, db_index=True)
+    last_name = models.CharField(max_length=100, db_index=True)
+    username = models.CharField(max_length=100, db_index=True, unique=True)
+
+
+class TelegramMessage(models.Model):
+    message_id = models.IntegerField()
+    user = models.ForeignKey(TelegramUser, on_delete=models.CASCADE)
+    date = models.DateTimeField()
+    text = models.TextField()
+
+
+class ChatSerializer(models.Model):
+    type = models.CharField(max_length=50, db_index=True)
