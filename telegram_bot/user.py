7c5610fc9d8540e6c11e1cba8bece512cf65441d
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-09-09 21:43:30 +0300

    refactoring

--- telegram_bot/user.py
+++ telegram_bot/user.py
@@ -1,5 +1,6 @@
 import re
 
+from telegram_bot.handlers.handlers import UpdateHandler
 from webhook.models import TelegramUser, TelegramMessage
 from webhook.serializers import UpdateSerializer
 
@@ -10,84 +11,6 @@ def initialized(method):
         self.initialized = True
 
     return decorator
-
-
-class MessageHandler:
-
-    def __init__(self, update: UpdateSerializer):
-        self.update = update
-
-    def get_user_id(self):
-        return self.update.data['message']['user'].get('id')
-
-    def get_first_name(self):
-        return self.update.data['message']['user'].get('first_name')
-
-    def get_last_name(self):
-        return self.update.data['message']['user'].get('last_name')
-
-    def get_username(self):
-        return self.update.data['message']['user'].get('username')
-
-    def get_text(self):
-        return self.update.data['message'].get('text')
-
-    def get_message_id(self):
-        return self.update.data['message'].get('message_id')
-
-
-class CallBackHandler:
-
-    def __init__(self, update: UpdateSerializer):
-        self.update = update
-
-    def get_user_id(self):
-        return self.update.data['callback_query']['user'].get('id')
-
-    def get_first_name(self):
-        return self.update.data['callback_query']['user'].get('first_name')
-
-    def get_last_name(self):
-        return self.update.data['callback_query']['user'].get('last_name')
-
-    def get_username(self):
-        return self.update.data['callback_query']['user'].get('username')
-
-    def get_text(self):
-        return None
-
-    def get_message_id(self):
-        return self.update.data['callback_query']['message'].get('message_id')
-
-
-class UpdateHandler:
-
-    def __init__(self, update: UpdateSerializer):
-        self.update = update
-        if self.update.data.get('message'):
-            self.type = "message"
-            self.handler = MessageHandler(update)
-        elif self.update.data.get('callback_query'):
-            self.type = "callback"
-            self.handler = CallBackHandler(update)
-
-    def get_user_id(self):
-        return self.handler.get_user_id()
-
-    def get_first_name(self):
-        return self.handler.get_first_name()
-
-    def get_last_name(self):
-        return self.handler.get_last_name()
-
-    def get_username(self):
-        return self.handler.get_username()
-
-    def get_text(self):
-        return self.handler.get_text()
-
-    def get_message_id(self):
-        return self.handler.get_message_id()
 
 
 class User:
