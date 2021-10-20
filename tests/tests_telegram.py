3164f552b823c3749da1399ffee3bd0cfff416e1
Дмитрий <55105946+isys35@users.noreply.github.com>
2021-10-20 21:00:20 +0300

    change test

--- tests/tests_telegram.py
+++ tests/tests_telegram.py
@@ -11,8 +11,8 @@ class WelcomeTest(TestCase):
 
     def test_send_photo(self):
         context = TelegramContext(settings.TELEGRAM_TOKEN)
-        BASE_DIR = pathlib.Path(__file__).parent.parent.parent
-        path_file = os.path.join(BASE_DIR, r'media/images/test-image.jpg')
+        BASE_DIR = pathlib.Path(__file__).parent
+        path_file = os.path.join(BASE_DIR, r'test-image.jpg')
         photo = open(path_file, 'rb')
         context.send_photo(settings.TEST_USER_ID, photo, 'Описание')
         photo.close()
